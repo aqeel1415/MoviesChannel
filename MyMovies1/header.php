95b10cbd8bbaed9f3a8128e929621ec2c73b4972
aqeel1415 <47818385+aqeel1415@users.noreply.github.com>
2019-02-26 13:28:24 +0300

    Update header.php

--- MyMovies1/header.php
+++ MyMovies1/header.php
@@ -12,7 +12,6 @@
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
-  <script src="https://code.jquery.com/jquery-1.11.1.min.js"></script>
   <script src="scripts/expander.js"></script>
   </head>
   <body>
