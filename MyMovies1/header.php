503ae0edc5130a94069a614aa1bc19f6ca1c0d5c
aqeel1415 <47818385+aqeel1415@users.noreply.github.com>
2019-02-26 11:51:50 +0300

    Update header.php

--- MyMovies1/header.php
+++ MyMovies1/header.php
@@ -8,8 +8,6 @@
       <?php echo $sitename ?>  
       <?php echo $tagline ?>
     </title>
-    
-  <link href="css/Style.css" rel="stylesheet">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
