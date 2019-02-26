7c9ce1d54500d5c117cdc6d0393189372f48a4c2
aqeel1415 <47818385+aqeel1415@users.noreply.github.com>
2019-02-26 21:52:33 +0300

    Update index.php

--- MyMovies1/index.php
+++ MyMovies1/index.php
@@ -1,4 +1,5 @@
 <link href="css/Style.css" rel="stylesheet">
+<link href="css/SlideShow.css" rel="stylesheet">
 <?php
   include "conf/info.php";
   $title="Welcome to ";
@@ -25,48 +26,17 @@
 <h1>The Top Rated Movies </h1>
 <hr> 
 
-<div class="container">
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-    <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <li data-target="#myCarousel" data-slide-to="1"></li>
-      <li data-target="#myCarousel" data-slide-to="2"></li>
-    </ol>
-
-    <!-- Wrapper for slides -->
-    <div class="carousel-inner">
-        <?php 
-            $hasAddedActive = false;
+ <div class="container">
+  <div class="row">
+      <?php
             foreach($toprated->results as $p){     
-        ?>
-        <?php 
-            $divClass = 'item';
-            $divClass .= $hasAddedActive ? '' : ' active';
-            $hasAddedActive = true;
-            echo('<div class="'.$divClass.'">'); 
-        ?>
-       <?php   
-            echo '<a href="movie.php?id=' . $p->id . '"><img  src="'.$imgurl_2.''. $p->poster_path . '"><h4>' . $p->original_title . " (" . substr($p->release_date, 0, 4) . ")</h4><h5><em>Rate : " . $p->vote_average . " |  Vote : " . $p->vote_count . "</em></h5></a>";
-        ?>
-        <?php echo('</div>'); ?>    
-        
-        <?php }
-        
-        ?>
+            echo '<div class="col-sm"><a href="movie.php?id=' . $p->id . '"><img  src="'.$imgurl_2.''. $p->poster_path . '"><h4>' . $p->original_title . " (" . substr($p->release_date, 0, 4) . ")</h4><h5><em>Rate : " . $p->vote_average . " |  Vote : " . $p->vote_count . "</em></h5></a></div>";
 
-    </div>
-    <!-- Left and right controls -->
-    <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-      <span class="glyphicon glyphicon-chevron-left"></span>
-      <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="#myCarousel" data-slide="next">
-      <span class="glyphicon glyphicon-chevron-right"></span>
-      <span class="sr-only">Next</span>
-    </a>
+            }
+      ?> 
   </div>
 </div>
+
 
 <?php
   include_once "footer.php";
