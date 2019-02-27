c85a7dd401fea3629ae4be0ff3cb8ef9c5197bb8
aqeel1415 <47818385+aqeel1415@users.noreply.github.com>
2019-02-27 14:58:18 +0300

    Update header.php

--- MyMovies1/header.php
+++ MyMovies1/header.php
@@ -8,43 +8,42 @@
       <?php echo $sitename ?>  
       <?php echo $tagline ?>
     </title>
-    
-  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" >
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
   <script src="scripts/expander.js"></script>
   </head>
   <body>
   
-  <!-- navigation bar --> 
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <a class="navbar-brand" href="index.php">MoviesBox</a>
-  <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
-    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
-      <li class="nav-item ">
-        <a class="nav-link" href="index.php">Home <span class="sr-only">Home</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="now-playing.php">Latest Movies</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="Favorites.php">My Favorites</a>
-      </li>
-    </ul>
-      <ul class="nav navbar-nav navbar-right">
-      <li>
-      <form class="form-inline my-2 my-lg-0"  action="search.php" method="get">
-      <input class="form-control mr-sm-2" name="search" type="search" placeholder="Search Movies" required aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-    </li>
-    </ul> 
-  </div>
-</nav>
+<div class="bs-example">
+    <nav class="navbar navbar-default">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a href="index.php" class="navbar-brand">MoviesBox</a>
+        </div>
+        <!-- Collection of nav links and other content for toggling -->
+        <div id="navbarCollapse" class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="index.php">Home <span class="sr-only">Home</span></a></li>
+                <li><a href="now-playing.php">Latest Movies</a></li>
+                <li><a href="Favorites.php">My Favorites</a></li>
+            </ul>
+            <ul class="nav navbar-nav navbar-right">
+                 <li>
+                     <form class="form-inline my-2 my-lg-0"  action="search.php" method="get">
+                     <input class="form-control mr-sm-2" name="search" type="search" placeholder="Search Movies" required aria-label="Search">
+                     <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                    </form>
+                </li>
+            </form>
+            </ul>
+        </div>
+    </nav>
+</div>
 
-
-
-   
